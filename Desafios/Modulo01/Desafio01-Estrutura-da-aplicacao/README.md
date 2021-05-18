05cd3d9f370b40f3a079ad4bc929193e9f71bcae
Wescley Torres <jewescley@gmail.com>
2021-05-17 21:00:50 -0300

    Desafio01: Estrutura da aplicacao📱

---
+++ Desafios/Modulo01/Desafio01-Estrutura-da-aplicacao/README.md
@@ -0,0 +1,3 @@
+# 💻 Sobre o desafio
+
+Nesse desafio você deverá reestruturar o aplicativo construído durante as aulas, separando os widgets que estão no arquivo `main.dart` afim de melhorar a compreensão do código, isolando as responsabilidades em diferentes arquivos.
---
+++ Desafios/Modulo01/Desafio01-Estrutura-da-aplicacao/meu_primeiro_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'Home/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
